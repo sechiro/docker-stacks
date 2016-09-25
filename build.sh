acaed18bc83d91f4661f7d73b646b0b5f56e38ee
Cloud User <centos@ip-10-0-0-201.ap-northeast-1.compute.internal>
2016-09-25 12:29:39 +0000

    CentOS 7 with ODBC first commit

---
+++ build.sh
@@ -0,0 +1,6 @@
+docker build -t sechiro/base-notebook base-notebook/
+docker build -t sechiro/minimal-notebook minimal-notebook/ 
+docker build -t sechiro/scipy-notebook scipy-notebook/
+docker build -t sechiro/my-datascience-notebook my-datascience-notebook/
+#docker build -t sechiro/pyspark-notebook pyspark-notebook
+#docker build -t sechiro/all-spark-notebook all-spark-notebook
